f4ea27950b763a8c1994e55698e4507228b56173
Abdelali12-codes <jadelmoulaa3@gmail.com>
2021-08-23 15:59:45 +0100

    new changes

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -19,11 +19,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
-app.use(express.static(path.join(__dirname, '../build')));
-app.use('/public', express.static(path.join(__dirname, '../public')));
+
 app.use('/api', router);
-app.get('*', (req, res) => {
-  res.sendFile('build/index.html', {root: path.join(__dirname, '../')});
-});
+
 
 module.exports = app;
