df77205b660f152bfe4d9cd627da22536ccbf8b2
Abdelali Jadelmoula <67081878+Abdelali12-codes@users.noreply.github.com>
2021-08-24 15:59:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends PureComponent {
           </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
-          Abdelali
+         React js Starter
          
         </Footer>
       </Layout>
